b1100b2a937f8509b0bef818c8b1282343fff6a9
Jeff Hammond <jeff.science@gmail.com>
2021-07-21 13:19:49 +0300

    add runStdParVariant boilerplate everywhere

--- src/apps/MASS3DPA.hpp
+++ src/apps/MASS3DPA.hpp
@@ -377,6 +377,7 @@ public:
   void runCudaVariant(VariantID vid);
   void runHipVariant(VariantID vid);
   void runOpenMPTargetVariant(VariantID vid);
+  void runStdParVariant(VariantID vid);
 
 private:
 
--- src/polybench/POLYBENCH_GEMVER.hpp
+++ src/polybench/POLYBENCH_GEMVER.hpp
@@ -152,6 +152,7 @@ public:
   void runCudaVariant(VariantID vid);
   void runHipVariant(VariantID vid);
   void runOpenMPTargetVariant(VariantID vid);
+  void runStdParVariant(VariantID vid);
 
 private:
   Index_type m_n;
