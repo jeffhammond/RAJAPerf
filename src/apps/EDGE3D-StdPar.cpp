3092febc48d2b0ff49501590114f88e95c753cd4
Jeff Hammond <jehammond@nvidia.com>
2023-10-19 03:40:14 -0700

    EDGE3D-StdPar.cpp

---
+++ src/apps/EDGE3D-StdPar.cpp
@@ -0,0 +1,71 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2017-23, Lawrence Livermore National Security, LLC
+// and RAJA Performance Suite project contributors.
+// See the RAJAPerf/LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#include "EDGE3D.hpp"
+
+#if defined(BUILD_STDPAR)
+
+#include "RAJA/RAJA.hpp"
+
+#include "common/StdParUtils.hpp"
+
+#include "AppsData.hpp"
+
+#include <iostream>
+
+namespace rajaperf
+{
+namespace apps
+{
+
+
+void EDGE3D::runStdParVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune_idx))
+{
+  const Index_type run_reps = getRunReps();
+  const Index_type ibegin = m_domain->fpz;
+  const Index_type iend = m_domain->lpz+1;
+
+  EDGE3D_DATA_SETUP;
+
+  auto edge3d_lam = [=](Index_type i) {
+                     EDGE3D_BODY;
+                   };
+
+  switch ( vid ) {
+
+    case Base_StdPar : {
+
+      startTimer();
+      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
+
+        for (Index_type i = ibegin ; i < iend ; ++i ) {
+          EDGE3D_BODY;
+        }
+
+      }
+      stopTimer();
+
+      break;
+    }
+
+    default : {
+      getCout() << "\n  EDGE3D : Unknown variant id = " << vid << std::endl;
+    }
+
+  }
+
+#else
+  RAJA_UNUSED_VAR(vid);
+#endif
+}
+
+} // end namespace apps
+} // end namespace rajaperf
+
+#endif  // BUILD_STDPAR
+
